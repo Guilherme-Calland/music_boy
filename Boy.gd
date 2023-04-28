405cdb661ebe0036069b41a9e08bc2f06bcbbd60
Guilherme <gui.crb0@gmail.com>
2023-04-28 18:00:20 -0300

    back to basics

--- Boy.gd
+++ Boy.gd
@@ -1,17 +1,10 @@
 extends KinematicBody2D
 
 enum Note {
-	c,d,e,f,g,null
+	c,cSh,d,dSh,e,f,fSh,g,gSh,a,aSh,b
 }
 
 var note = Note.c
 
-func _process(delta):
-	
-	if Input.is_action_pressed("c"):
-		if note != Note.c:
-			$Voice.play()
-		note = Note.c
-	else:
-		$Voice.stop()
-		note = null
+func run():
+	pass
